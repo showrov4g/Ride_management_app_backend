795428f9af45de2b6bd73efca95cd68d431b632c
showrov4g <showrovg00@gmail.com>
2025-08-04 21:21:24 +0600

    app.ts file setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
 import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+app.use(cookieParser())
+
+// routes 
+
+app.get('/',(req, res)=>{
+    res.send("Ride Booking API is Running")
+})
+
+export default app;
+
 
 
